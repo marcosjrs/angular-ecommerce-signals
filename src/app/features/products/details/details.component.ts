a815ac524e5ba9299ba250262aeb207c72bf9497
MarcosJRS <marcosjrs80@gmail.com>
2024-03-10 11:15:15 +0100

    Uso de valores del Signal de producto en el html y añadir html dinámico en el componente con [innerHTML], es este caso para las estrellas del rating

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -16,7 +16,7 @@ export default class DetailsComponent implements OnInit{
   // @Input({ alias: 'id' }) productId!: number;
   productId = input<number>(0, { alias: 'id' });
   product!: Signal<Product | undefined>;
-
+  
   private readonly _sanitizer = inject(DomSanitizer);
   private readonly productsSvc = inject(ProductsService);
   
@@ -25,6 +25,37 @@ export default class DetailsComponent implements OnInit{
   }
 
   onAddToCart() {
+  }
+
+  generateSVG(index: number): SafeHtml {
+    let svgContent = null;
+
+    const rate = this.product()?.rating.rate as number;
+
+    if (index + 1 <= Math.floor(rate)) {
+      svgContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+              stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+              </path>
+            </svg>`;
+    } else if (index < rate) {
+      svgContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+          <defs>
+            <linearGradient id="partialFillGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="50%" style="stop-color:currentColor; stop-opacity:1" />
+              <stop offset="50%" style="stop-color:currentColor; stop-opacity:0" />
+            </linearGradient>
+          </defs>
+          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="url(#partialFillGradient)"></path>
+        </svg>`;
+    } else {
+      svgContent = `<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+              </path>
+            </svg>`;
+    }
+    return this._sanitizer.bypassSecurityTrustHtml(svgContent);
   }
 
 }
