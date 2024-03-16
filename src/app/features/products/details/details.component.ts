bd3bfff8a6c3b98411a7f3de4da10d4324c3f355
MarcosJRS <marcosjrs80@gmail.com>
2024-03-16 09:04:05 +0100

    Add checkout

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Signal, inject, input } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ProductsService } from '@api/products.service';
 import { Product } from '@shared/models/product.interface';
+import { CartStore } from '@shared/store/cart.store';
 
 @Component({
   selector: 'app-details',
@@ -10,21 +11,23 @@ import { Product } from '@shared/models/product.interface';
   imports: [CurrencyPipe],
   templateUrl: './details.component.html',
 })
-export default class DetailsComponent implements OnInit{
+export default class DetailsComponent implements OnInit {
   starsArray: number[] = new Array(5).fill(0);
-  
+
   // @Input({ alias: 'id' }) productId!: number;
   productId = input<number>(0, { alias: 'id' });
   product!: Signal<Product | undefined>;
-  
+  cartStore = inject<any>(CartStore);
+
   private readonly _sanitizer = inject(DomSanitizer);
   private readonly productsSvc = inject(ProductsService);
-  
+
   ngOnInit(): void {
     this.product = this.productsSvc.getProductById(this.productId());
   }
 
   onAddToCart() {
+    this.cartStore.addToCart(this.product() as Product);
   }
 
   generateSVG(index: number): SafeHtml {
