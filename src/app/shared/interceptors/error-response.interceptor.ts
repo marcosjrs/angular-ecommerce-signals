a931ff4ad3731df1fe2091d41dce5b81628abf3a
MarcosJRS <marcosjrs80@gmail.com>
2024-03-16 08:58:22 +0100

    Tipado correcto del error interceptor

--- src/app/shared/interceptors/error-response.interceptor.ts
+++ src/app/shared/interceptors/error-response.interceptor.ts
@@ -1,12 +1,20 @@
-import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 
-export const ErrorResponseInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req)
-    .pipe(catchError(handleErrorResponse));
-};
+export const ErrorResponseInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => next(req).pipe(catchError(handleErrorResponse));
 
-function handleErrorResponse( error: HttpErrorResponse): ReturnType<typeof throwError> {
-  return throwError( ()=>`Error code: ${error.status}, message: ${error.message}` )
+function handleErrorResponse(
+  error: HttpErrorResponse
+): ReturnType<typeof throwError> {
+  const errorResponse = `Error code :${error.status}, message: ${error.message}`;
+  return throwError(() => errorResponse);
 }
 
